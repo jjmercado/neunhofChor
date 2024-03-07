e24810e3aeaaf0f76ab29c8e2339fa3584bb65ed
Jeremy Mercado <jeremy.j.mercado@hotmail.com>
2024-03-07 23:57:09 +0100

    Change layout

--- handwriting.js
+++ handwriting.js
@@ -1,16 +1,18 @@
-const text1 = "Liebst du es, mit anderen zu singen und magische Harmonien zu erleben?";
-const text2 = "Möchtest du neue Freundschaften knüpfen und deine Stimme verbessern?";
-const text3 = "Fühlst du die Begeisterung am Singen und möchtest diese Freude teilen?";
-const character = text1.split(" ");
+const words = ["Harmonie", "Gemeinschaft", "Leidenschaft", "Magie"]
 const speed = 100; // Geschwindigkeit, mit der die Wörter geschrieben werden (in Millisekunden)
-
 let i = 0;
-function typeWriter() {
-  if (i < character.length) {
-    document.getElementById("text").innerHTML += character[i] + " ";
-    i++;
-    setTimeout(typeWriter, speed);
-  }
+
+function typeWriter() 
+{
+    let span = document.getElementById("word-1");
+    let word = words[0];
+    
+    if(span.innerHTML.length != words[0].length)
+    {
+      span.innerHTML += word[i];
+      i++;
+      setTimeout(typeWriter, speed);
+    }  
 }
 
-window.onload = typeWriter;
+window.onload = typeWriter; 
