9e16ed525fdb601a016567d67bb4d3e2739dbd18
Jeremy Mercado <jeremy.j.mercado@hotmail.com>
2024-03-08 14:02:06 +0100

    Change layout and organize css

---
+++ textScroll.js
@@ -0,0 +1,16 @@
+window.addEventListener('scroll', function() 
+{
+    var element = document.getElementById('text-scroll');
+    var position = element.getBoundingClientRect();
+  
+    // Überprüfen, ob der Text im sichtbaren Bereich liegt
+    if (position.top >= 0 && position.bottom <= window.innerHeight) 
+    {
+      // Vergrößere den Text
+      element.style.fontSize = '4rem'; // Ändere die gewünschte Größe hier
+    } else {
+      // Setze die Größe auf die Ausgangsgröße zurück
+      element.style.fontSize = '1rem'; // Setze die ursprüngliche Größe hier
+    }
+});
+  
