3580635d1ad9d9ce0bd9bed03067e2533be96586
Jeremy Mercado <jeremy.j.mercado@hotmail.com>
2025-01-21 15:33:18 +0100

    correct weird zoom behaviour

--- scripts/scrollTop.js
+++ scripts/scrollTop.js
@@ -19,4 +19,5 @@ function scrollFunction() {
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  document.scrollingElement.scrollTop = 0;
 }
