c13e8140d9ea84d431a994b199d90f1cdebdc01e
Jeremy Mercado <jeremy.j.mercado@hotmail.com>
2025-01-21 22:52:20 +0100

    adjust all for media querie

--- scripts/textScroll.js
+++ scripts/textScroll.js
@@ -1,16 +1,43 @@
-window.addEventListener('scroll', function() 
-{
-    var element = document.getElementById('text-scroll');
-    var position = element.getBoundingClientRect();
-  
-    // Überprüfen, ob der Text im sichtbaren Bereich liegt
-    if (position.top >= 0 && position.bottom <= window.innerHeight) 
+if(window.matchMedia("(min-width: 320px)") && window.matchMedia("(max-width: 768px)"))
+  {
+    window.addEventListener('scroll', function() 
     {
-      // Vergrößere den Text
-      element.style.fontSize = '3rem'; // Ändere die gewünschte Größe hier
-    } else {
-      // Setze die Größe auf die Ausgangsgröße zurück
-      element.style.fontSize = '1rem'; // Setze die ursprüngliche Größe hier
-    }
-});
+        var element = document.getElementById('text-scroll');
+        var position = element.getBoundingClientRect();
+      
+        // Überprüfen, ob der Text im sichtbaren Bereich liegt
+        if (position.top >= 0 && position.bottom <= window.innerHeight) 
+        {
+          // Vergrößere den Text
+          element.style.fontSize = '2rem'; // Ändere die gewünschte Größe hier
+        }
+        else 
+        {
+          // Setze die Größe auf die Ausgangsgröße zurück
+          element.style.fontSize = '1rem'; // Setze die ursprüngliche Größe hier
+        }
+    });
+  }
   
+
+if(window.matchMedia("(min-width: 1300px)"))
+{
+  window.addEventListener('scroll', function() 
+  {
+      var element = document.getElementById('text-scroll');
+      var position = element.getBoundingClientRect();
+    
+      // Überprüfen, ob der Text im sichtbaren Bereich liegt
+      if (position.top >= 0 && position.bottom <= window.innerHeight) 
+      {
+        // Vergrößere den Text
+        element.style.fontSize = '2rem'; // Ändere die gewünschte Größe hier
+      }
+      else 
+      {
+        // Setze die Größe auf die Ausgangsgröße zurück
+        element.style.fontSize = '1rem'; // Setze die ursprüngliche Größe hier
+      }
+  });
+}
+
